6b995dd6d4cacc2b99daa2a1ed789e0cc0ec4f52
Pablo Lopez <pflopez@gmail.com>
2018-04-19 15:40:06 -0300

    Added the missing test on service.spec using spies

--- js808/src/app/services/sequencer.service.spec.ts
+++ js808/src/app/services/sequencer.service.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed, inject, fakeAsync, tick, async } from '@angular/core/testing';
 import { SequencerService } from "./sequencer.service";
 import { FormsModule } from "@angular/forms";
+import Spy = jasmine.Spy;
 
 let service: SequencerService;
-
+let successFn: Spy;
 
 describe('SequencerService', () => {
 	beforeEach(() => {
@@ -13,6 +14,7 @@ describe('SequencerService', () => {
 		});
 
 		service = TestBed.get(SequencerService);
+		successFn = jasmine.createSpy('spy');
 	});
 	
 
@@ -30,17 +32,68 @@ describe('SequencerService', () => {
 		testBpm(120);
 		testBpm(132);
 	}) );
+	
+	it('should be triggered the right number of beats per minute',  fakeAsync(  ()=>{
+		expect(successFn).not.toHaveBeenCalled();
+		service.sequence.subscribe( successFn);
+
+		let lastTimeCalled = 1;
+
+		// first time called with -1 (stop)
+		expect(successFn).toHaveBeenCalledWith(-1);
+		expect(successFn).toHaveBeenCalledTimes(lastTimeCalled);
+
+		service.setBpm(60);
+		service.start();
+		tick( 1000 );
+		/*
+		First time called with stop (1) when subscribed
+		Then 4 beats + last beat
+		(or beat 0 + 4)
+
+		           0     0.25   0.50   0.75   1.00
+		 |.........|------|------|------|------|........
+		-1         1      2      3      4      5
+		 */
+
+		lastTimeCalled = lastTimeCalled + 4 + 1;
+		expect(successFn).toHaveBeenCalledTimes(lastTimeCalled);
+		expect(successFn.calls.mostRecent().args[0]).toBe(4);
+
+		tick( 2000 );
+		lastTimeCalled = lastTimeCalled + 8 ;
+		expect(successFn).toHaveBeenCalledTimes(lastTimeCalled);
+		expect(successFn.calls.mostRecent().args[0]).toBe( 4 + 8 );
+
+		service.stop();
+		// should add one more for the stop
+		expect(successFn).toHaveBeenCalledTimes(lastTimeCalled + 1);
+		expect(successFn.calls.mostRecent().args[0]).toBe( -1);
+
+		successFn.calls.reset();
+		lastTimeCalled = 0;
+		expect(successFn).toHaveBeenCalledTimes(lastTimeCalled);
+
+		service.setBpm(120);
+		service.start();
+		tick( 1000 );
+		lastTimeCalled = 8 + 1;
+		expect(successFn).toHaveBeenCalledTimes(lastTimeCalled);
+		expect(successFn.calls.mostRecent().args[0]).toBe( 8 );
+		tick( 1000 );
+		lastTimeCalled = 8 + lastTimeCalled;
+		expect(successFn).toHaveBeenCalledTimes( lastTimeCalled );
+		// 8 + 8 = 16  -> full bar, reset counter to  0.
+		expect(successFn.calls.mostRecent().args[0]).toBe( 0);
+		service.stop();
+		expect(successFn).toHaveBeenCalledTimes( lastTimeCalled + 1 );
+		expect(successFn.calls.mostRecent().args[0]).toBe( -1);
+
+	}) );
 
 	it('should provide correct step number based on BPM time', fakeAsync( () => {
 
 		let step = 0;
-		// I wanted this test to be a little different, testing the 
-		// number of times the subscriber was called.
-		// I was not able to get the successFn.toHaveBeenCalledTimes to make sense.
-		// let successFn = jasmine.createSpy('spy');
-		// So I'm testing that Im getting the correct step number instead.
-		
-
 		let sequence = service.sequence.subscribe( (v) => step = v );
 
 		service.setBpm(60);
